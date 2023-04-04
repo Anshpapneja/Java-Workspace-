ed178bd5b1eb1ad2b6a95fe00ad985798fa04403
Anshpapneja <Anshpapneja01@gmail.com>
2023-04-04 23:26:32 +0530

    04-04-23

---
+++ operator.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class operator {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println("add =" + (A+B));
+    }    
+}
