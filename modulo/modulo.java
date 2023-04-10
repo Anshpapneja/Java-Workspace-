c9c5d398703780b5c8d303ac00b88419cb4164a3
Anshpapneja <128931565+Anshpapneja@users.noreply.github.com>
2023-04-10 23:26:49 +0530

    Create modulo.java

---
+++ modulo/modulo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class modulo {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+         System.out.println("modulo(remainder) = " + (A%B));
+    }    
+}
